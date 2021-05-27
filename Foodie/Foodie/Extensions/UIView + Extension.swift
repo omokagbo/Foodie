378b051f8e52cb7892d97370646ca62def3fadcc
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-05-28 00:42:56 +0100

    create outlets for onboarding screen

---
+++ Foodie/Foodie/Extensions/UIView + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView + Extension.swift
+//  Foodie
+//
+//  Created by omokagbo on 28/05/2021.
+//
+
+import Foundation
--- Foodie/Foodie/Views/Onboarding/OnboardingViewController.swift
+++ Foodie/Foodie/Views/Onboarding/OnboardingViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+       
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
     }
     
 }
