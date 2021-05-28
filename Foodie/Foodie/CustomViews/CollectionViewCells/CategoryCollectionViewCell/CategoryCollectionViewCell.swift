e31b7432cf574d84882bf0a4ba3e2106b39cbf6e
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-05-28 23:39:57 +0100

    add Kingfisher via SPM and create cell for DishCategory

--- Foodie/Foodie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
+++ Foodie/Foodie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryImgView: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func setup(_ category: DishCategory) {
+        categoryImgView.image = UIImage(named: category.image)
+        categoryTitleLbl.text = category.name
     }
 
 }
---
+++ Foodie/Foodie/Models/DishCategory.swift
@@ -0,0 +1,12 @@
+//
+//  DishCategory.swift
+//  Foodie
+//
+//  Created by omokagbo on 28/05/2021.
+//
+
+import UIKit
+
+struct DishCategory {
+    let id, name, image: String
+}
