262f379cf59a78092c603d23a14ea5db6486dbdc
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-05-28 23:10:42 +0100

    create category collection view cell

---
+++ Foodie/Foodie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Foodie
+//
+//  Created by Decagon on 28/05/2021.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
