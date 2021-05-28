0a6dc61a78121bef24426e6a0e3aee30048d8e97
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-05-28 02:31:05 +0100

    onboarding screen complete

--- Foodie/Foodie/Views/Onboarding/OnboardingViewController.swift
+++ Foodie/Foodie/Views/Onboarding/OnboardingViewController.swift
@@ -14,11 +14,38 @@ class OnboardingViewController: UIViewController {
     @IBOutlet weak var pageControl: UIPageControl!
     private var viewModel = OnboardingViewModel()
     
+    private var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == viewModel.slides.count - 1 {
+                nextButton.setTitle("Get Started", for: .normal)
+            } else {
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel.slides = [
+            OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: #imageLiteral(resourceName: "slide2")),
+            OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: #imageLiteral(resourceName: "slide1")),
+            OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: #imageLiteral(resourceName: "slide3"))
+        ]
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
+        if currentPage == viewModel.slides.count - 1 {
+            guard let homeNC = storyboard?.instantiateViewController(withIdentifier: "HomeNC") as? UINavigationController else { return }
+            homeNC.modalPresentationStyle = .fullScreen
+            homeNC.modalTransitionStyle = .flipHorizontal
+            present(homeNC, animated: true, completion: nil)
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
     }
     
 }
@@ -31,6 +58,7 @@ extension OnboardingViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as? OnboardingCollectionViewCell else { return UICollectionViewCell() }
+        cell.setup(viewModel.slides[indexPath.row])
         return cell
     }
     
@@ -40,3 +68,16 @@ extension OnboardingViewController: UICollectionViewDelegate {
     
 }
 
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+    }
+    
+}
