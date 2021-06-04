4304256a069ed86371f10308c9fcb3a6f784d2bb
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-06-04 19:05:08 +0100

    update configuration for IQKeyboard

--- Foodie/Foodie/App + Scene Delegates/AppDelegate.swift
+++ Foodie/Foodie/App + Scene Delegates/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
         UINavigationBar.appearance().shadowImage = UIImage()
         UINavigationBar.appearance().tintColor = .black
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableDebugging = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
     
