e0c24f186e94f84972c8aabfb8134a4de8d22924
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-06-13 20:06:04 +0100

    create view for orders

---
+++ Foodie/Foodie/Orders/OrdersViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OrdersViewController.swift
+//  Foodie
+//
+//  Created by omokagbo on 13/06/2021.
+//
+
+import UIKit
+
+class OrdersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
