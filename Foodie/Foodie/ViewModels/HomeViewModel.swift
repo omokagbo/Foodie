18636647724c0030438fdf0f9223d279eb5c2c50
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-08-05 08:25:05 +0100

    refactored Onboarding View Controller to MVVM

---
+++ Foodie/Foodie/ViewModels/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Foodie
+//
+//  Created by omokagbo on 05/08/2021.
+//
+
+import Foundation
--- Foodie/Foodie/ViewModels/OnboardingViewModel.swift
+++ Foodie/Foodie/ViewModels/OnboardingViewModel.swift
@@ -8,9 +8,27 @@
 import Foundation
 
 class OnboardingViewModel {
-    var slides = [
-        OnboardingSlideModel(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: #imageLiteral(resourceName: "slide2")),
-        OnboardingSlideModel(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: #imageLiteral(resourceName: "slide1")),
-        OnboardingSlideModel(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: #imageLiteral(resourceName: "slide3"))
-    ]
+    
+    var slides: [OnboardingSlideModel] = []
+    
+    var updateButton: ((String, Int, String) -> Void)?
+    
+    var currentPage = 0 {
+        didSet {
+            if currentPage == slides.count - 1 {
+                updateButton?("Get Started", currentPage, "")
+            } else {
+                updateButton?("Next", currentPage, "Skip")
+            }
+        }
+    }
+    
+    init() {
+        slides = [
+            OnboardingSlideModel(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: #imageLiteral(resourceName: "slide2")),
+            OnboardingSlideModel(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: #imageLiteral(resourceName: "slide1")),
+            OnboardingSlideModel(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: #imageLiteral(resourceName: "slide3"))
+        ]
+    }
+    
 }
